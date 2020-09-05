40dd2c06beb0dee561b7d2ec6dcb379de8e797f6
Harminder Singh <singh.harminder05@gmail.com>
2020-09-05 12:50:55 +1200

    Add project files.

---
+++ SpecflowTest/Pages/LoginPage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using OpenQA.Selenium;
+
+namespace SpecflowTest.Pages
+{
+    public class LoginPage
+    {
+        public IWebDriver WebDriver { get; set; }
+        public LoginPage(IWebDriver webDriver)
+        {
+            WebDriver = webDriver;
+        }
+
+        public IWebElement lnkLogin => WebDriver.FindElement(By.LinkText("Login"));
+        public IWebElement txtUserName => WebDriver.FindElement(By.Name("UserName"));
+        public IWebElement txtPassword => WebDriver.FindElement(By.Name("Password"));
+        public IWebElement btnLogin => WebDriver.FindElement(By.CssSelector(".btn-default"));
+        public IWebElement lnkEmployeeDetails => WebDriver.FindElement(By.LinkText("Employee Details"));
+
+        public void ClickLogin()
+        {
+            lnkLogin.Click();
+        }
+
+        public void Login(string userName, string password)
+        {
+            txtUserName.SendKeys(userName);
+            txtPassword.SendKeys(password);
+        }
+
+        public void ClickLoginButton()
+        {
+            btnLogin.Submit();
+        }
+
+        public bool IsEmployeeDetailsExist()
+        {
+            return lnkEmployeeDetails.Displayed;
+        }
+
+    }
+}
